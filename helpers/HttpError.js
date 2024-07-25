71af759716bd4cb9f77a23f9ba2ef1d86c70c308
didukhroma <didukhroma@gmail.com>
2024-07-25 06:46:37 +0300

    fix: add husky settings

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,9 +1,9 @@
 import HttpCode from './HttpCode.js';
 
 const HttpError = (status, message = HttpCode[status]) => {
-  const error = new Error(message);
-  error.status = status1;
-  return error;
+	const error = new Error(message);
+	error.status = status;
+	return error;
 };
 
 export default HttpError;
